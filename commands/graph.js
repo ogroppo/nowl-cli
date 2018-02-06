3255cbc2184f3ef7509748662a77c361c25fa83d
Orlando Groppo <>
2018-02-06 14:52:07 +0000

    include ascii graph

--- commands/graph.js
+++ commands/graph.js
@@ -1,5 +1,5 @@
 const chalk = require('chalk')
-const asciiGraph = require('ascii-graph')
+const asciiGraph = require('../lib/asciiGraph')
 
 const parseCliNode = require('../lib/parseCliNode')
 const deleteNode = require('../lib/deleteNode')
@@ -7,11 +7,17 @@ const formatNode = require('../lib/formatNode')
 const graphNode = require('../lib/graphNode')
 
 exports.handler = function graphCommand(argv) {
-	let nodeToGraph = parseCliNode(process.argv[3])
-	graphNode(nodeToGraph).then(jsonGraph => {
+	let startDate = new Date
+	let rootNode = parseCliNode(argv.node)
+	graphNode(rootNode).then(jsonGraph => {
+		console.info(chalk.green('Your Tree'), `in ${getDateDiffSeconds(startDate)}s`);
 		//console.log(JSON.stringify(jsonGraph, null, 2))
 		asciiGraph(jsonGraph)
 	}).catch(e => console.info(e))
 }
 
 exports.builder = {}
+
+function getDateDiffSeconds(startDate){
+	return ((new Date - startDate) / 1000).toFixed(1)
+}
