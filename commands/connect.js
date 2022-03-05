f2c4cf2b8e56ca5434d5ffbbb9b39eaec509640f
Orlando Groppo <ogroppo@apple.com>
2022-03-05 11:33:29 +0000

    stuff

--- commands/connect.js
+++ commands/connect.js
@@ -1,11 +1,16 @@
-const md5 = require('md5');
 const chalk = require('chalk');
 
-const authInterface = require('../interfaces/authInterface')
+const authInterface = require('../interfaces/auth')
 const login = require('../lib/login')
 const getDomain = require('../lib/getDomain')
 
 exports.handler = async function connect(argv) {
+
+	var domain = await getDomain(argv.domain)
+	if(!domain){
+		console.log(chalk.red("Domain not found, set it up"))
+		process.exit(1)
+	}
 
 	var email, password;
 	if(!argv.email || !argv.password){
@@ -21,12 +26,14 @@ exports.handler = async function connect(argv) {
 		console.log(chalk.red(`User does not exist`))
 		process.exit(1)
 	}
-	//
-	// var domain = await getDomain(argv.domain, user)
-	// if(!domain)
-	// 	return console.log(chalk.red("Domain not found, set it up"))
-	//
-	// domainInterface(user, domain)
+
+	var permission = await getDomainPermission(domain, user)
+	if(!permission){
+		console.log(chalk.red("You don't have any permission on this Domain"))
+		process.exit(1)
+	}
+
+	shell()
 }
 
 exports.builder = {}
